2d18e2a273724a8b71e3895f4525fd561d4ca375
ehsanrazakazmi <ehsankazmi7@gmail.com>
2024-06-27 17:42:04 +0500

    pushed
    pushed

---
+++ src/components/Admin.jsx
@@ -0,0 +1,14 @@
+import AdminHello from "./AdminHello"
+
+const Admin = () => {
+    return (
+        <main className="admin">
+
+            <AdminHello />
+
+            <h2>Admin</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, aperiam esse molestiae, laborum reiciendis suscipit doloribus blanditiis corrupti dolorem iure beatae placeat tempora voluptatum magni consectetur fugit at! Suscipit natus deleniti molestiae repudiandae reprehenderit fugit enim repellendus officiis est nisi vel blanditiis delectus at placeat necessitatibus earum architecto eligendi, fugiat sint consequatur ipsam. Corrupti quae nihil odit aliquam nostrum omnis, nesciunt ducimus dignissimos voluptates voluptatibus ullam fugit voluptas voluptatum beatae earum fugiat sapiente? Dolor qui eaque omnis voluptatum rem itaque, consectetur velit mollitia veritatis praesentium ad minima quisquam facilis molestias totam asperiores fugit doloremque quas eveniet et cupiditate a quidem!</p>
+        </main>
+    )
+}
+export default Admin
